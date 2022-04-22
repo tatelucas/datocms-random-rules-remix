cc644acf3d53c7b6b27c94013dfdb76d0749d07b
Tate Lucas <tate.lucas@gmail.com>
2022-04-21 21:02:33 -0400

    h1 and tagline

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -54,8 +54,8 @@ export default function Index() {
     <div className="container">
       <section className="section">
         <div className="flex">
-          <h1 className="title">Remix Blog</h1>
-          <h4 className="subtitle">A blog example using Remix and DatoCMS.</h4>
+          <h1 className="title">Random Rules</h1>
+          <h4 className="subtitle">Web3 in 3 minutes.</h4>
         </div>
       </section>
       <section className="section">
